f34aaf47ea33efc9c6624eae96d4de7aa19c2a41
mayconasilva <mayconsilva0381@gmail.com>
2021-10-06 11:53:22 -0300

    Comandos Slash readcionados = adicionado comando de simulado

---
+++ comandos/simulados.js
@@ -0,0 +1,90 @@
+const Discord = require("discord.js")
+const DiscordBuildes = require("@discordjs/builders")
+module.exports = {
+    data: new DiscordBuildes.SlashCommandBuilder()
+        .setName("simulados")
+        .setDescription("Este comando serve para você testar os seus conhecimentos")
+        .addStringOption(option => option.setName('disciplina').setDescription('Me diga qual a disciplica')),
+
+    async execute(interaction) {
+
+        const disciplica = interaction.options.getString('disciplina');
+
+        if (disciplica == "português" || disciplica == "lplb" || disciplica == "portugues") {
+
+            function selecionar() {
+
+                let quiz = require("./simulados/lplb.json")
+                const item = quiz[Math.floor(Math.random() * quiz.length)];
+                return item
+            }
+
+            const filter = () => true
+
+            let res = []
+            let ans = []
+            let pontos = 0
+
+            let item = selecionar()
+
+            ans.push(item.answers)
+            interaction.reply("**Orientações para o simulado ** \n 1 - Serão 5 questões objetivas de diferentes provas e simulados \n 2 - Para os pontos serem contabilizados é necessário que você escreva a letra da questão em maiúsculo. Por exemplo, a resposta certa era a letra 'a', você irá escrever apenas 'A' \n 3 - Não há tempo limite para resposta, mas tente responder em 3 minutos \n 4 - Boa sorte")
+            interaction.followUp(item.question, { fetchReply: true })
+                .then(() => {
+                    interaction.channel.awaitMessages({ filter, max: 1 })
+                        .then(m => {
+                            res.push(m.map(a => a.content))
+                            let item2 = selecionar()
+                            ans.push(item2.answers)
+                            interaction.followUp(item2.question, { fetchReply: true })
+                                .then(() => {
+                                    interaction.channel.awaitMessages({ filter, max: 1 })
+                                        .then(r => {
+                                            res.push(r.map(a => a.content))
+                                            let item3 = selecionar()
+                                            ans.push(item3.answers)
+                                            interaction.followUp(item3.question, { fetchReply: true })
+                                                .then(() => {
+                                                    interaction.channel.awaitMessages({ filter, max: 1 })
+                                                        .then(p => {
+                                                            res.push(p.map(a => a.content))
+                                                            let item4 = selecionar()
+                                                            ans.push(item4.answers)
+                                                            interaction.followUp(item4.question, { fetchReply: true })
+                                                                .then(() => {
+                                                                    interaction.channel.awaitMessages({ filter, max: 1 })
+                                                                        .then(l => {
+                                                                            res.push(l.map(a => a.content))
+                                                                            let item5 = selecionar()
+                                                                            ans.push(item5.answers)
+                                                                            interaction.followUp(item5.question, { fetchReply: true })
+                                                                                .then(() => {
+                                                                                    interaction.channel.awaitMessages({ filter, max: 1 })
+                                                                                        .then(w => {
+                                                                                            res.push(w.map(a => a.content))
+
+                                                                                            if (res[0] == ans[0]) { pontos + 1 } if (res[1] == ans[1]) { pontos + 1 } if (res[2] == ans[2]) { pontos + 1 } if (res[3] == ans[3]) { pontos + 1 } if (res[4] == ans[4]) { pontos + 1 }
+                                                                                            let embed = new Discord.MessageEmbed()
+                                                                                                .setTitle("Resultado")
+                                                                                                .setColor("#0B00C8")
+                                                                                                .setDescription(`Você acertou ${pontos.toString()}/5. As respostas das perguntas corretas eram ${ans}, respectivamente`)
+                                                                                            interaction.followUp({ embeds: [embed] })
+                                                                                        })
+                                                                                })
+                                                                        })
+                                                                })
+                                                        })
+                                                })
+
+                                        })
+                                }
+                                )
+                        })
+                })
+
+        } else {
+            interaction.reply(`Olá ${interaction.user.tag}!Como vai você? \n Para iniciar o simulado, por favor diga qual a disciplica. \n As opções disponíveis atualmente são: \n -lplb`)
+        }
+
+    }
+}
--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const { Client, Intents, Guild, Message, Collection, MessageActionRow, MessageButton } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const fs = require('fs');
+
 
 const client = new Client({ intents: 32767 });
 const config = require("./config.json");
+
+const comma = []
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./comandos').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./comandos/${file}`);
+	client.commands.set(command.data.name, command);
+  comma.push(command.data.toJSON());
+  console.log(`Comando ${command.data.name} carregado`)
+}
+
+const rest = new REST({ version: '9' }).setToken(config.token);
+
+(async () => {
+	try {
+		console.log('Iniciando carregamento dos comandos slash');
+
+		await rest.put(
+			Routes.applicationGuildCommands(config.clientID, config.guildID),
+			{ body: comma },
+		);
+    
+
+		console.log('Os comandos foram carregados com sucesso');
+	} catch (error) {
+		console.error(error);
+	}
+})();
 
 client.once("ready", () => {
   console.log('Vinde Espírito Santo, enchei os corações dos vossos fiéis e acendei neles o fogo do Vosso Amor. Enviai o Vosso Espírito e tudo será criado e renovareis a face da terra. \n Oremos: Ó Deus que instruíste os corações dos vossos fiéis, com a luz do Espírito Santo, fazei que apreciemos retamente todas as coisas segundo o mesmo Espírito e gozemos da sua consolação.Por Cristo Senhor Nosso. Amém.')
@@ -38,6 +72,22 @@ client.on("messageCreate", async message => {
   } //comadno handler
 
 })
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+  //pode tirarm, só para verificação
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'Desculpe-me, não foi possivel realizar esse comando', ephemeral: true });
+	}
+});
 
 client.login(config.token)
 
